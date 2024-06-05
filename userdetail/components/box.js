b0baedab61239d40b19694ccc5f089868e493cd7
Ankit Khanal <akhanal749@gmail.com>
2024-06-05 21:10:20 +0545

    add

--- userdetail/components/box.js
+++ userdetail/components/box.js
@@ -4,16 +4,20 @@ import Image from 'next/image';
 export default function Box(){
     const [user, setUser] = useState('');
     const [result, setResult] = useState(null);
+    const [loading, setLoading] = useState(false);
     function setUserName(e){
         setUser(e.target.value);
     }
     const search = async  (e) => {
         e.preventDefault();
+        setLoading(true);
         const response = await fetch("https://api.github.com/users/"+user);   
         const data = await response.json();
         console.log(result);
         setResult(data);
+        setLoading(false);
     }
+
     return(
         <>
             <form id="form">
@@ -21,6 +25,13 @@ export default function Box(){
                 <input type="text" name="input" id="input" onChange={setUserName}/>
                 <input type="submit" value="Search" id="submit" onClick={search}/>
             </form>
+            {
+                loading && <p style={{
+                    textAlign: "center",
+                    fontSize: "1rem",
+                
+                }}>Loading...</p>
+            }
             {result &&
                 <div className="user">
                     <div className="photo">
