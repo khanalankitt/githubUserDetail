03f690804050707de90b4e859f6bd63cd6224f3a
Ankit Khanal <akhanal749@gmail.com>
2024-06-05 20:47:03 +0545

    added

--- userdetail/components/box.js
+++ userdetail/components/box.js
@@ -1,8 +1,10 @@
 "use client"
 import { useState } from "react";
-let result = {};
+import Image from 'next/image';
+let result;
 export default function Box(){
-    const [user,setUser] = useState('');
+    const [user, setUser] = useState('');
+    const [data,setData] = useState(false);
     function setUserName(e){
         setUser(e.target.value);
     }
@@ -10,7 +12,8 @@ export default function Box(){
         e.preventDefault();
         const response = await fetch("https://api.github.com/users/"+user);   
         result = await response.json();
-        console.log(result)
+        console.log(result);
+        setData(true);
     }
     return(
         <>
@@ -19,7 +22,23 @@ export default function Box(){
                 <input type="text" name="input" id="input" onChange={setUserName}/>
                 <input type="submit" value="Search" id="submit" onClick={search}/>
             </form>
+            {data &&
+                <div className="user">
+                    <div className="photo">
+                        <Image 
+                            src={`${result.avatar_url}`}
+                            alt="user" 
+                            width="100"
+                            height="100"
+                        />     
+                        <h2>{result.name}</h2>   
+                        <p>{result.bio}</p>
+                        <p>
+                            <span>{result.follower}</span> followers
+                           | <span>{result.following}</span> following</p>
+                    </div>
+                </div>
+            }  
         </>
     );
 }
-export  const res = result;
--- userdetail/components/user.js
+++ userdetail/components/user.js
@@ -1,12 +1,13 @@
 import Image from "next/image";
-import { res } from "./box";
+import { res } from "./box.js";
 export default function User(){
+    let result = res;
     return(
         <>
             <div className="user">
                 <div className="photo">
                     <Image 
-                        src={res.avatar_url}
+                        src={result.avatar_url}
                         alt="user" 
                         width="100"
                         height="100"
